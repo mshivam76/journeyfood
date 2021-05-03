5286c3b7fec8a4b0959c42e7a5b5a14485d84833
mshivam76 <mshivam76@gmail.com>
2021-05-03 17:14:23 +0530

    Initial commit

---
+++ src/test/java/org/brahmakumaris/journeyfood/JourneyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brahmakumaris.journeyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
