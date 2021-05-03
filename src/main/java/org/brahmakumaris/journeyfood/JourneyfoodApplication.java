ad48a8027f633a313240b293b031da24347d1001
mshivam76 <mshivam76@gmail.com>
2021-05-03 17:05:40 +0530

    Initial commit

---
+++ src/main/java/org/brahmakumaris/journeyfood/JourneyfoodApplication.java
@@ -0,0 +1,13 @@
+package org.brahmakumaris.journeyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyfoodApplication.class, args);
+	}
+
+}
